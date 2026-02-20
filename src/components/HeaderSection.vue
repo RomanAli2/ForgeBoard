bf6f55cfadd21efb08a4a1d9baf925d5edd72723
RomanAli2 <romanali87866677@gmail.com>
2026-02-20 01:59:55 -0800

    'first'

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -24,7 +24,7 @@
         <div v-if="HeaderToggleMode" class="flex gap-2 mt-2 md:mt-0 z-50 md:z-0 flex-col md:flex-row items-center">
           <button
            :class="ThemeStore.isDark ?  'hover:bg-slate-800' :'hover:bg-slate-200'" 
-          @click="LoginPagebtn"  class="px-4  py-2 md:py-2 w-full md:w-auto hover:bg-slate-400 rounded-xl">
+          @click="LoginPagebtn"  class="px-4  py-2 md:py-2 w-full md:w-auto  rounded-xl">
             <i class="fa-solid fa-arrow-right-to-bracket"></i> Sign in
           </button>
           <button :class="ThemeStore.isDark? ' text-black':' text-white'" class="px-4   py-2 md:py-2 w-full md:w-auto bg-blue-500 font-medium hover:bg-blue-600 rounded-xl">
@@ -36,8 +36,6 @@
             {{ isDark ? '🌙' : '☀️' }}
           </button>
         </div>
-       
-
       </nav>
     </header>
   </div>
